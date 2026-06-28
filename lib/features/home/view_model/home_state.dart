af351113fdcfd6392705bc84fbb13e5ee4a0a0ea
shahd_ahmed <shahdahmed8883@gmail.com>
2026-06-28 23:29:07 +0300

    add cubit and state to home

--- lib/features/home/view_model/home_state.dart
+++ lib/features/home/view_model/home_state.dart
@@ -1,24 +1,3 @@
-/*
-part of 'home_cubit.dart';
-
-sealed class HomeState {}
-
-final class HomeInitial extends HomeState {}
-
-final class HomeLoading extends HomeState {}
-
-final class HomeSuccess extends HomeState {}
-
-final class HomeError extends HomeState {
-  final String message;
-
-  HomeError(this.message);
-}
-
- */
-
-
-
 
 import '../data/model/task_model.dart';
 abstract class HomeState {}
