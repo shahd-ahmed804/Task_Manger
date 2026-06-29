2eeb52593fbb4f25839fb99c238dd0897b26e50d
shahd_ahmed <shahdahmed8883@gmail.com>
2026-06-29 16:35:05 +0300

    add shared_preferences

--- lib/core/widget/onboarding_model.dart
+++ lib/core/widget/onboarding_model.dart
@@ -1,11 +1,11 @@
 class OnBoardingData {
   String title;
-  String descrebtion;
+  String describtion;
   String image;
   OnBoardingData({
     required this.title,
     required this.image,
-    required this.descrebtion,
+    required this.describtion,
   });
 }
 
@@ -14,19 +14,19 @@ List<OnBoardingData> dataOnBoarding() {
     OnBoardingData(
       title: 'Manage your tasks',
       image: 'assets/images/on_boarding1.png',
-      descrebtion:
+      describtion:
           'You can easily manage all of your daily tasks in DoMe for free',
     ),
     OnBoardingData(
       title: 'Create daily routine',
       image: 'assets/images/on_boarding2.png',
-      descrebtion:
+      describtion:
           'In Tasky  you can create your personalized routine to stay productive',
     ),
     OnBoardingData(
       title: 'Orgonaize your tasks',
       image: 'assets/images/on_boarding3.png',
-      descrebtion:
+      describtion:
           'You can organize your daily tasks by adding your tasks into separate categories',
     ),
   ];
--- lib/onboarding_screen.dart
+++ lib/onboarding_screen.dart
@@ -1,3 +1,4 @@
+/*
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:taskey_app/const.dart';
@@ -67,7 +68,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
             ),
             SizedBox(height: 42),
             Text(
-              onBoarding[index].descrebtion,
+              onBoarding[index].describtion,
               style: TextStyle(
                 fontSize: 16,
                 fontWeight: FontWeight.w500,
@@ -86,6 +87,127 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                       curve: Curves.linear,
                     );
                   } else {
+                    Navigator.of(
+                      context,
+                    ).pushReplacementNamed(LoginScreen.routeName);
+                  }
+                },
+                color: Color(0xff5F33E1),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadiusGeometry.circular(10),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Text(
+                    index < onBoarding.length - 1 ? 'Next' : 'Get Started',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xffFFFFFF),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+ */
+import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:taskey_app/const.dart';
+import 'package:taskey_app/core/widget/custom_animated_widget.dart';
+import 'package:taskey_app/core/widget/onboarding_model.dart';
+
+import 'features/auth/view/screens/login_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+
+class OnboardingScreen extends StatefulWidget {
+  const OnboardingScreen({super.key});
+  static const String routeName = 'OnboardingScreen';
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  List<OnBoardingData> onBoarding = dataOnBoarding();
+  int index = 0;
+  PageController controller = PageController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffFFFFFF),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 40.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 260,
+              child: PageView.builder(
+                controller: controller,
+                onPageChanged: (value) {
+                  setState(() {
+                    index = value;
+                  });
+                },
+                itemBuilder: (context, index) => CustomAnimatedWidget(
+                  index: index,
+                  delay: index,
+                  child: Image.asset(onBoarding[index].image),
+                ),
+              ),
+            ),
+            SizedBox(height: 23),
+            SmoothPageIndicator(
+              controller: controller,
+              count: onBoarding.length,
+              effect: ExpandingDotsEffect(
+                dotWidth: 16,
+                dotHeight: 4,
+                dotColor: Color(0xffB9B9B9),
+                activeDotColor: themeColor,
+              ),
+            ),
+            SizedBox(height: 50),
+            Text(
+              onBoarding[index].title,
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Color(0xff24252C),
+              ),
+            ),
+            SizedBox(height: 42),
+            Text(
+              onBoarding[index].describtion,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+                color: Color(0xff6E6A7C),
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 95),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: MaterialButton(
+                onPressed: ()async {
+                  if (index < onBoarding.length - 1) {
+                    controller.nextPage(
+                      duration: Duration(milliseconds: 500),
+                      curve: Curves.linear,
+                    );
+                  } else {
+                    final prefs= await SharedPreferences.getInstance();
+                    await prefs.setBool('onBoardingDone', true);
+                    if(!mounted) return;
                     Navigator.of(
                       context,
                     ).pushReplacementNamed(LoginScreen.routeName);
